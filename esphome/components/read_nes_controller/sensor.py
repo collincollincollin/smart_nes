ccae65916b11d3bfa059d03de7e740e72ec0debd
Collin Jensen <collin.b.jensen@gmail.com>
2024-01-05 14:44:26 -0700

    added codegen section

--- esphome/components/read_nes_controller/sensor.py
+++ esphome/components/read_nes_controller/sensor.py
@@ -1,4 +1,5 @@
 import esphome.config_validation as cv
+import esphome.codegen as cg
 
 CONF_MY_REQUIRED_KEY = 'my_required_key'
 CONF_MY_OPTIONAL_KEY = 'my_optional_key'
@@ -7,3 +8,9 @@ CONFIG_SCHEMA = cv.Schema({
   cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
   cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
 }).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var)
+
+    cg.add(var.set_my_required_key(config[CONF_MY_REQUIRED_KEY]))
