3d5efd86c41325e23d3119ac64601d166db2ef56
Collin Jensen <collin.b.jensen@gmail.com>
2024-01-05 14:36:48 -0700

    add config validation

--- esphome/config_validation.py
+++
@@ -1,9 +0,0 @@
-import esphome.config_validation as cv
-
-CONF_MY_REQUIRED_KEY = 'my_required_key'
-CONF_MY_OPTIONAL_KEY = 'my_optional_key'
-
-CONFIG_SCHEMA = cv.Schema({
-  cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
-  cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
-}).extend(cv.COMPONENT_SCHEMA)
