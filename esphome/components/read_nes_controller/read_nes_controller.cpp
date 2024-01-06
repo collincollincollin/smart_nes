d57c3dfa3a256a4acc61a9797b28c670589c2662
Collin Jensen <collin.b.jensen@gmail.com>
2024-01-05 18:49:39 -0700

    commit message

--- esphome/components/read_nes_controller/read_nes_controller.cpp
+++ esphome/components/read_nes_controller/read_nes_controller.cpp
@@ -1,21 +1,63 @@
-#include "read_nes_controller.h"
+#include "nes_controller.h"
+#include "esphome/core/log.h"
 
-NESController::NESController(int data_pin, int clock_pin, int latch_pin) :
-  data_pin_(data_pin), clock_pin_(clock_pin), latch_pin_(latch_pin) {}
+namespace esphome {
+namespace nes_controller {
+
+static const char *const TAG = "nes_controller";
 
 void NESController::setup() {
-  pinMode(data_pin_, INPUT);
-  pinMode(clock_pin_, OUTPUT);
-  pinMode(latch_pin_, OUTPUT);
-  digitalWrite(clock_pin_, LOW);
-  digitalWrite(latch_pin_, LOW);
+  pinMode(this->data_pin_, INPUT);
+  pinMode(this->clock_pin_, OUTPUT);
+  pinMode(this->latch_pin_, OUTPUT);
+
+  digitalWrite(this->clock_pin_, LOW);
+  digitalWrite(this->latch_pin_, LOW);
 }
 
 void NESController::loop() {
-  nes_register_ = readNesController();
-  // Process button presses here or emit events
+  byte nes_register = read_nes_controller();
+
+  this->a_button_sensor->publish_state(!bitRead(nes_register, 0));
+  this->b_button_sensor->publish_state(!bitRead(nes_register, 1));
+  // ... Publish state for other buttons ...
+
+  delay(180);  // Slight delay to prevent spam
 }
 
-byte NESController::readNesController() {
-  // Your existing Arduino code goes here, adapted for ESPHome
+void NESController::set_data_pin(uint8_t data_pin) {
+  this->data_pin_ = data_pin;
 }
+
+void NESController::set_clock_pin(uint8_t clock_pin) {
+  this->clock_pin_ = clock_pin;
+}
+
+void NESController::set_latch_pin(uint8_t latch_pin) {
+  this->latch_pin_ = latch_pin;
+}
+
+byte NESController::read_nes_controller() {
+  byte temp_data = 0xFF;
+
+  pulse_pin(this->latch_pin_);
+  
+  for (int i = 0; i < 8; i++) {
+    if (digitalRead(this->data_pin_) == LOW)
+      bitClear(temp_data, i);
+
+    pulse_pin(this->clock_pin_);
+  }
+
+  return temp_data;
+}
+
+void NESController::pulse_pin(uint8_t pin) {
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(12);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(12);
+}
+
+}  // namespace nes_controller
+}  // namespace esphome
--- esphome/components/read_nes_controller/read_nes_controller.h
+++ esphome/components/read_nes_controller/read_nes_controller.h
@@ -1,20 +1,32 @@
-#include "esphome.h"
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+
+namespace esphome {
+namespace nes_controller {
 
 class NESController : public Component {
  public:
-  // Constructor
-  NESController(int data_pin, int clock_pin, int latch_pin);
-
-  // ESPHome methods
   void setup() override;
   void loop() override;
 
-  // Custom method to read the controller
-  byte readNesController();
+  void set_data_pin(uint8_t data_pin);
+  void set_clock_pin(uint8_t clock_pin);
+  void set_latch_pin(uint8_t latch_pin);
+
+  binary_sensor::BinarySensor *a_button_sensor = new binary_sensor::BinarySensor();
+  binary_sensor::BinarySensor *b_button_sensor = new binary_sensor::BinarySensor();
+  // ... Define sensors for other buttons ...
 
  private:
-  int data_pin_;
-  int clock_pin_;
-  int latch_pin_;
-  byte nes_register_;
+  uint8_t data_pin_;
+  uint8_t clock_pin_;
+  uint8_t latch_pin_;
+
+  byte read_nes_controller();
+  void pulse_pin(uint8_t pin);
 };
+
+}  // namespace nes_controller
+}  // namespace esphome
