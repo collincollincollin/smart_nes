70c85d4cd32d567d395f249ea95cc266e4da869b
Collin Jensen <collin.b.jensen@gmail.com>
2024-01-05 19:16:52 -0700

    fixed names

--- esphome/components/nes_controller/sensor.py
+++ esphome/components/nes_controller/sensor.py
@@ -1,23 +1,22 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 
-CONF_MY_REQUIRED_KEY = 'my_required_key'
-DATA_PIN = 'data pin'
-CLOCK_PIN = 'clock pin'
-LATCH_PIN = 'latch pin'
+# CONF_MY_REQUIRED_KEY = 'my_required_key'
+DATA_PIN = 'data_pin'
+CLOCK_PIN = 'clock_pin'
+LATCH_PIN = 'latch_pin'
 
 CONF_MY_OPTIONAL_KEY = 'my_optional_key'
 A_BUTTON = 'A Button'
 B_BUTTON = 'B Button'
 
 CONFIG_SCHEMA = cv.Schema({
-  cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
   cv.Required(DATA_PIN): cv.string,
   cv.Required(CLOCK_PIN): cv.string,
   cv.Required(LATCH_PIN): cv.string,
   cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
-  cv.Optional(A_BUTTON, default=10): cv.string,
-  cv.Optional(B_BUTTON, default=10): cv.string,
+  cv.Optional(A_BUTTON, default="?"): cv.string,
+  cv.Optional(B_BUTTON, default="?"): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 # def to_code(config):
